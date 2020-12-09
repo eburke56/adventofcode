807766cfafa9208324dbb508be4d9b0687bf4ca4
Eric Burke <erburke@linkedin.com>
2020-12-09 07:10:46 -0500

    Day 9 part 2

--- 2020/src/day09/Day09.kt
+++ 2020/src/day09/Day09.kt
@@ -15,22 +15,48 @@ private fun isSumOfTwoNumbers(list: List<Long>, number: Long): Boolean {
     return false
 }
 
-private fun findFirstFailure(filename: String, preambleLength: Int): Long {
+private fun findFirstFailure(filename: String, preambleLength: Int): Pair<Long, List<Long>> {
     val input = readInput(filename)
+    val inputCopy = input.toList()
 
     while (input.size > preambleLength) {
         val testList = input.take(preambleLength)
         val testValue = input[preambleLength]
         if (!isSumOfTwoNumbers(testList, testValue)) {
-            return testValue
+            return Pair(testValue, inputCopy)
         }
         input.removeAt(0)
     }
 
-    return -1
+    return Pair(-1, listOf())
+}
+
+private fun findEncryptionFailure(filename: String, preambleLength: Int): Long {
+    val (failValue, list) = findFirstFailure(filename, preambleLength)
+    var total = list[0]
+    var start = 0
+
+    for (i in 1 until list.size) {
+        if (total == failValue) {
+            return list.subList(start, i).let {
+                checkNotNull(it.min()) + checkNotNull(it.max())
+            }
+        }
+
+        total += list[i]
+
+        while (total > failValue) {
+            total -= list[start]
+            start++
+        }
+    }
+
+    return -1L
 }
 
 fun main() {
-    println("Test: ${findFirstFailure("test.txt", 5)}")
-    println("Read: ${findFirstFailure("input.txt", 25)}")
+    println("Test: ${findFirstFailure("test.txt", 5).first}")
+    println("Real: ${findFirstFailure("input.txt", 25).first}")
+    println("Test: ${findEncryptionFailure("test.txt", 5)}")
+    println("Real: ${findEncryptionFailure("input.txt", 25)}")
 }
