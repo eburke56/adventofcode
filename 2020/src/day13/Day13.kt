c97e9c7d1ad167942571a714ad4edd5e7a34f1f0
Eric Burke <erburke@linkedin.com>
2020-12-13 09:25:33 -0500

    day 13 part 2 brute

--- 2020/src/day13/Day13.kt
+++ 2020/src/day13/Day13.kt
@@ -1,32 +1,81 @@
 package day12
 
 import util.readAllLines
-import kotlin.math.min
-
-private fun readInput(filename: String): Pair<Int, List<Int>> {
-    val input = readAllLines(filename)
-    return Pair(
-        input[0].toInt(),
-        input[1].split(",").mapNotNull { it.toIntOrNull() }
-    )
-}
 
 private fun findFirstBus(filename: String) {
-    val (timestamp, buses) = readInput(filename)
+    val input = readAllLines(filename)
+    val timestamp = input[0].toInt()
+    val buses = input[1].split(",").mapNotNull { it.toIntOrNull() }
     var bestId = -1
     var bestArrival = Int.MAX_VALUE
-    buses.forEach { id ->
-        val nextArrival = timestamp + id - (timestamp % id)
-        println ("Next arrival for bus $id --> $nextArrival")
-        if (nextArrival < bestArrival) {
-            bestId = id
-            bestArrival = nextArrival
+    buses
+        .forEach { id ->
+            val nextArrival = timestamp + id - (timestamp % id)
+            println("Next arrival for bus $id --> $nextArrival")
+            if (nextArrival < bestArrival) {
+                bestId = id
+                bestArrival = nextArrival
+            }
+        }
+    println("First bus $filename: ts = $timestamp, id = $bestId, nextArrival = $bestArrival --> ${bestId * (bestArrival - timestamp)}")
+}
+
+private fun findSequentialBuses(filename: String) {
+    val input = readAllLines(filename)
+    val buses = input[1].split(",").map { if (it == "x") 0 else it.toInt() }
+    val filteredBuses = buses.filter { it > 0 }
+    val maxBus = checkNotNull(filteredBuses.max())
+    var timestamp = - buses.indexOf(maxBus).toLong()
+
+    println ("Buses: ${buses.joinToString()}")
+    println ("Bus 0: ${buses[0]}")
+    println ("Filtered: ${filteredBuses.joinToString()}")
+
+    var found = false
+    while (!found) {
+        timestamp += maxBus
+        found = true
+//        println ("Test: ts = $timestamp")
+        for (currentBusId in filteredBuses) {
+            val currentBusIndex = buses.indexOf(currentBusId).toLong()
+//            println ("    testing id = $currentBusId, index = $currentBusIndex, mod = ${(timestamp + currentBusIndex) % currentBusId}")
+            if ((timestamp + currentBusIndex) % currentBusId != 0L) {
+//                println ("    bail")
+                found = false
+                break
+            }
         }
     }
-    println ("First bus $filename: ts = $timestamp, id = $bestId, nextArrival = $bestArrival --> ${bestId * (bestArrival - timestamp)}")
+
+//    sortedOtherBuses.forEach { currentBusId ->
+//        println("Testing bus $currentBusId with increment $increment")
+//        val otherBusIndex = buses.indexOf(currentBusId).toLong()
+//        while (true) {
+//            timestamp += increment
+////            println ("    Testing timestamp $timestamp: mod = ${timestamp % id}, obi = $otherBusIndex")
+//            if (timestamp % currentBusId == otherBusIndex) {
+//                // make sure no buses land in any X row between this row and the 0th row of this set
+//                val zero = timestamp - otherBusIndex
+//                xIndicies.forEach { xIndex ->
+//                    sortedOtherBuses.any { otherBusId -> (zero + xIndex) % otherBusId == buses.indexOf(otherBusId).toLong() } )
+//                }
+//                println("    Found bus $currentBusId with index $otherBusIndex at timestamp $timestamp, isAlsoInAnX = $isAlsoInAnX")
+//
+//                if (!isAlsoInAnX) {
+//                    break
+//                }
+//            }
+//        }
+//        increment = currentBusId
+//        println ("    New increment is $increment")
+//    }
+
+    println ("Sequential $filename: $timestamp")
 }
 
 fun main() {
-    findFirstBus("test.txt")
-    findFirstBus("input.txt")
+//    findFirstBus("test.txt")
+//    findFirstBus("input.txt")
+    findSequentialBuses("test.txt")
+    findSequentialBuses("input.txt")
 }
