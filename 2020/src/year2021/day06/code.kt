74f6b1bbb7cab93089a98b038fabce2839e1f024
Eric Burke <erburke@linkedin.com>
2021-12-06 08:44:58 -0500

    2021 day 6

--- 2020/src/year2021/day06/code.kt
+++ 2020/src/year2021/day06/code.kt
@@ -2,57 +2,26 @@ package year2021.day06
 
 import util.readAllLines
 
-private class Lanternfish(initialValue: Int) {
-    var value: Int = initialValue
-        private set
-
-    fun advance(): Lanternfish? {
-        if (value == 0) {
-            value = 6
-            return Lanternfish(8)
-        }
-
-        value -= 1
-        return null
-    }
-}
-
 fun main() {
     part1()
     part2()
 }
 
 private fun part1() {
-    run2(80, "test.txt")
+    run(80, "input.txt")
 }
 
 private fun part2() {
-    val times = 256
-    val file = "input.txt"
-    run2(times, file)
+    run(256, "input.txt")
 }
 
 private fun run(times: Int, file: String) {
-    val school = mutableListOf<Lanternfish>().apply {
-        addAll(readAllLines(file).first().split(",").map { Lanternfish(it.toInt()) })
-    }
-    (1..times).forEach { iteration ->
-        val newFish = mutableListOf<Lanternfish>()
-        school.forEach { fish ->
-            fish.advance()?.let { newFish.add(it) }
-        }
-        school.addAll(newFish)
-    }
-    println("Algo 1: ${school.size}")
-}
-
-private fun run2(times: Int, file: String) {
     val size = readAllLines(file).first().split(",")
         .map { it.toInt() }
         .sumOf { value ->
             (1 + countSpawned(times, value % 7))
         }
-    println("Algo 2: $size")
+    println(size)
 }
 
 private val cache = mutableMapOf<Int, Long>()
