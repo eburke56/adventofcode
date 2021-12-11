6056b8472babcf396ba8011c493d0cb61a29a7dc
Eric Burke <erburke@linkedin.com>
2021-12-11 10:06:02 -0500

    2021 day 11 part 2

--- 2020/src/year2021/day11/code.kt
+++ 2020/src/year2021/day11/code.kt
@@ -1,83 +1,74 @@
 package year2021.day11
 
 import util.readAllLines
-import kotlin.math.max
 
-//private fun dumpGrid(grid: List<List<Octopus>>, label: Any) {
-//    if (label.toString().isNotEmpty())
-//        println("Iteration $label:")
-//    else
-//        println()
-//
-//    grid.forEach { row ->
-//        print("    ")
-//        row.forEach { octopus ->
-//            print(octopus.value)
-//        }
-//        println()
-//    }
-//}
-
-private fun step(grid: List<List<Octopus>>, iteration: Int, maxIterations: Int): Int {
-    if (iteration == 0) return 0
-
-    grid.forEach { row ->
-        row.forEach { octopus ->
-            octopus.increment()
-        }
-    }
-
-    return stepFlashed(grid, iteration, maxIterations)
-}
-
-private fun stepFlashed(grid: List<List<Octopus>>, iteration: Int, maxIterations: Int): Int {
-    if (iteration == 0) return 0
-
+private fun countFlashes(grid: List<List<Octopus>>, iteration: Int, maxIterations: Int): Int {
+    if (iteration > maxIterations) return 0
     var numFlashed = 0
-    var justFlashed = grid.flatMapIndexed { r, row ->
-        row.mapIndexedNotNull { c, octopus ->
-            if (octopus.value == 0) Pair(
-                r,
-                c
-            ) else null
-        }
-    }.toMutableSet()
+    var justFlashed = initialStep(grid)
 
     while (justFlashed.isNotEmpty()) {
-        val maxRow = grid.size - 1
         numFlashed += justFlashed.size
-        val newJustFlashed = mutableSetOf<Pair<Int, Int>>()
+        justFlashed = getJustFlashed(grid, justFlashed)
+    }
 
-        grid.forEachIndexed { rowIndex, row ->
-            val maxCol = row.size - 1
-            row.forEachIndexed { colIndex, octopus ->
-                if (octopus.value > 0) {
-                    var numAdjacentFlashed = 0
-                    for (r in rowIndex - 1..rowIndex + 1) {
-                        for (c in colIndex - 1..colIndex + 1) {
-                            if (r == rowIndex && c == colIndex) continue
-                            if (r < 0 || r > maxRow) continue
-                            if (c < 0 || c > maxCol) continue
-                            if (justFlashed.contains(Pair(r, c)) && grid[r][c].value == 0) {
-                                numAdjacentFlashed += 1
-                            }
-                        }
-                    }
-                    if (octopus.increment(numAdjacentFlashed)) {
-                        newJustFlashed.add(Pair(rowIndex, colIndex))
-                    }
-                }
-            }
-        }
+    return numFlashed + countFlashes(grid, iteration + 1, maxIterations)
+}
 
-        justFlashed = newJustFlashed
+private fun findSynced(grid: List<List<Octopus>>, iteration: Int) {
+    var justFlashed = initialStep(grid)
+
+    while (justFlashed.isNotEmpty()) {
+        justFlashed = getJustFlashed(grid, justFlashed)
     }
 
     if (grid.sumOf { row -> row.count { it.value > 0 } } == 0) {
-        println("ALL SYNCED AT ${maxIterations - iteration}")
+        println(iteration)
+    } else {
+        findSynced(grid, iteration + 1)
+    }
+}
+
+private fun initialStep(grid: List<List<Octopus>>): MutableSet<Pair<Int, Int>> {
+    grid.forEach { row -> row.forEach { octopus -> octopus.increment() } }
+
+    return grid.flatMapIndexed { r, row ->
+        row.mapIndexedNotNull { c, octopus ->
+            if (octopus.value == 0) Pair(r, c) else null
+        }
+    }.toMutableSet()
+}
+
+private fun getJustFlashed(
+    grid: List<List<Octopus>>,
+    justFlashed: MutableSet<Pair<Int, Int>>
+): MutableSet<Pair<Int, Int>> {
+    val maxRow = grid.size - 1
+    val newJustFlashed = mutableSetOf<Pair<Int, Int>>()
+
+    grid.forEachIndexed { rowIndex, row ->
+        val maxCol = row.size - 1
+        row.forEachIndexed { colIndex, octopus ->
+            if (octopus.value > 0) {
+                var numAdjacentFlashed = 0
+                for (r in rowIndex - 1..rowIndex + 1) {
+                    for (c in colIndex - 1..colIndex + 1) {
+                        if (r == rowIndex && c == colIndex) continue
+                        if (r < 0 || r > maxRow) continue
+                        if (c < 0 || c > maxCol) continue
+                        if (justFlashed.contains(Pair(r, c))) {
+                            numAdjacentFlashed += 1
+                        }
+                    }
+                }
+                if (octopus.increment(numAdjacentFlashed)) {
+                    newJustFlashed.add(Pair(rowIndex, colIndex))
+                }
+            }
+        }
     }
 
-    return numFlashed + step(grid, iteration - 1, maxIterations)
+    return newJustFlashed
 }
 
 class Octopus(initialValue: Int) {
@@ -91,22 +82,22 @@ class Octopus(initialValue: Int) {
 }
 
 fun main() {
-//    part1()
+    part1()
     part2()
 }
 
 private fun part1() {
-    val grid = readAllLines("input.txt").mapIndexed { rowIndex, line ->
-        line.mapIndexed { colIndex, char -> Octopus(char.digitToInt()) }
+    val grid = readAllLines("input.txt").map { line ->
+        line.map { char -> Octopus(char.digitToInt()) }
     }
 
-    println(step(grid, 100, 100))
+    println(countFlashes(grid, 1, 100))
 }
 
 private fun part2() {
-    val grid = readAllLines("test.txt").mapIndexed { rowIndex, line ->
-        line.mapIndexed { colIndex, char -> Octopus(char.digitToInt()) }
+    val grid = readAllLines("input.txt").map { line ->
+        line.map { char -> Octopus(char.digitToInt()) }
     }
 
-    println(step(grid, 100, 100))
+    findSynced(grid, 1)
 }
